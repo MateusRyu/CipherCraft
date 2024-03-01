104370fc06de2831c1c75c6f99c883af1f3e3643
Mateus Ryu Yamaguchi <mateus.ryu8@gmail.com>
2024-02-29 23:17:37 -0300

    fix: repara bug visual da saída da conversão

--- script.js
+++ script.js
@@ -6,93 +6,92 @@ const alerta__titulo = document.getElementById("alerta__titulo");
 const alerta__mensagem = document.getElementById("alerta__mensagem");
 
 const alertas = {
-    vazio: {
-        icone: "&nexist;",
-        titulo: "Nenhuma mensagem encontrada",
-        mensagem: "Digite um texto que você deseja criptografar ou descriptografar."
-    },
-    nenhum: {
-        icone: "",
-        titulo: "",
-        mensagem: ""
-    },
-    one: {
-        icone: "&#9888;",
-        titulo: "Mensagem inválida para conversão!",
-        mensagem: "A mensagem deve ter apenas letras minúsculas e sem acento."
-    }
-}
+  vazio: {
+    icone: "&nexist;",
+    titulo: "Nenhuma mensagem encontrada",
+    mensagem:
+      "Digite um texto que você deseja criptografar ou descriptografar.",
+  },
+  nenhum: {
+    icone: "",
+    titulo: "",
+    mensagem: "",
+  },
+  one: {
+    icone: "&#9888;",
+    titulo: "Mensagem inválida para conversão!",
+    mensagem: "A mensagem deve ter apenas letras minúsculas e sem acento.",
+  },
+};
 
 function exibeAlerta(tipo) {
-    let alerta = alertas[tipo];
-    alerta__icone.innerHTML = alerta.icone;
-    alerta__titulo.innerHTML = alerta.titulo;
-    alerta__mensagem.innerHTML = alerta.mensagem;
+  let alerta = alertas[tipo];
+  alerta__icone.innerHTML = alerta.icone;
+  alerta__titulo.innerHTML = alerta.titulo;
+  alerta__mensagem.innerHTML = alerta.mensagem;
 }
 
 function validaAlfabetoMinusculo(texto) {
-    return /^[a-z]+$/.test(texto);
+  return /^[a-z]+$/.test(texto);
 }
 
 const criptografias = {
-    "ONE": function (texto, reverso) {
-        if (texto=="") {
-            exibeAlerta("vazio");
-            return(false);
-        } else if (validaAlfabetoMinusculo(texto) == false) {
-            exibeAlerta("one");
-        } else if (reverso == false) {
-            cifra = texto.replaceAll("e", "enter");
-            cifra = cifra.replaceAll("i", "imes");
-            cifra = cifra.replaceAll("a", "ai");
-            cifra = cifra.replaceAll("o", "ober");
-            cifra = cifra.replaceAll("u", "ufat");
-        }  else if (reverso == true) {
-            cifra = texto.replaceAll("enter", "e");
-            cifra = cifra.replaceAll("imes", "i");
-            cifra = cifra.replaceAll("ai", "a");
-            cifra = cifra.replaceAll("ober", "o");
-            cifra = cifra.replaceAll("ufat", "u");
-        }
-
-        return cifra;
+  ONE: function (texto, reverso) {
+    if (texto == "") {
+      exibeAlerta("vazio");
+      return false;
+    } else if (validaAlfabetoMinusculo(texto) == false) {
+      exibeAlerta("one");
+    } else if (reverso == false) {
+      cifra = texto.replaceAll("e", "enter");
+      cifra = cifra.replaceAll("i", "imes");
+      cifra = cifra.replaceAll("a", "ai");
+      cifra = cifra.replaceAll("o", "ober");
+      cifra = cifra.replaceAll("u", "ufat");
+    } else if (reverso == true) {
+      cifra = texto.replaceAll("enter", "e");
+      cifra = cifra.replaceAll("imes", "i");
+      cifra = cifra.replaceAll("ai", "a");
+      cifra = cifra.replaceAll("ober", "o");
+      cifra = cifra.replaceAll("ufat", "u");
     }
+
+    return cifra;
+  },
 };
 
 function limpaCampo() {
-    entrada.value = "";
-    exibeAlerta("vazio");
+  entrada.value = "";
+  escondeElemento(saida);
+  exibeAlerta("vazio");
 }
 
-function escondeElemento(elemento){
-    if (elemento.classList.contains('escondido') == false) {
-        elemento.classList.add("escondido");
-    } 
+function escondeElemento(elemento) {
+  if (elemento.classList.contains("escondido") == false) {
+    elemento.classList.add("escondido");
+  }
 }
 
-
-
-function exibeElemento(elemento){
-    if (elemento.classList.contains("escondido") == true) {
-        elemento.classList.remove("escondido");
-    } 
+function exibeElemento(elemento) {
+  if (elemento.classList.contains("escondido") == true) {
+    elemento.classList.remove("escondido");
+  }
 }
 
-
-
-function criptografar(reverso=false) {
-    reverso = reverso ? true : false;
-    cifra = criptografias["ONE"](input.value, reverso);
-    if (cifra) {
-        exibeAlerta("nenhum");
-        exibeElemento(saida);
-        saida.innerHTML = cifra;
-    } 
+function criptografar(reverso = false) {
+  reverso = reverso ? true : false;
+  cifra = criptografias["ONE"](input.value, reverso);
+  if (cifra) {
+    exibeAlerta("nenhum");
+    exibeElemento(saida);
+    saida.innerHTML = cifra;
+  }
 }
-
 
 function copiar() {
-    window.navigator.clipboard.writeText(saida.innerText);
-    botao_copiar.innerText = "Copiado!";
-    setTimeout(() => {botao_copiar.innerText = "Copiar"}, 2000);
+  window.navigator.clipboard.writeText(saida.innerText);
+  botao_copiar.innerText = "Copiado!";
+  setTimeout(() => {
+    botao_copiar.innerText = "Copiar";
+  }, 2000);
 }
